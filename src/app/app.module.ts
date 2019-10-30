102feed7085cb505f07215a1214ecfb88d1f417e
Ethan Massie <ethan.massie1@gmail.com>
2019-10-30 17:40:52 -0500

    added home and about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
