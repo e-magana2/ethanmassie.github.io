90d189062492f43c67630557d28ff7ebe8079114
Ethan Massie <ethan.massie1@gmail.com>
2019-11-07 13:37:13 -0600

    changed color theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,6 +11,8 @@ import { AboutComponent } from './about/about.component';
 import { ExampleService } from './services/example.service';
 import { KanyeService } from './services/kanye.service';
 import { FooterComponent } from './footer/footer.component';
+import { ThemeModule } from './theme/theme.module';
+import { theme } from './theme/theme';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,11 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ThemeModule.forRoot({
+      themes: [theme],
+      active: 'theme'
+    })
   ],
   providers: [
     ExampleService,
