c5539736bc0419225400f192b0d72944971fa300
Ethan Massie <ethan.massie1@gmail.com>
2019-11-07 13:36:53 -0600

    added theming module

---
+++ src/app/theme/theme.ts
@@ -0,0 +1,15 @@
+import { Theme } from './symbols';
+
+export const theme: Theme = {
+  name: 'theme',
+  properties: {
+    '--background': '#22333B',
+    '--on-background': '#EAE0D5',
+    '--primary': '#131c20',
+    '--on-primary': '#EAE0D5',
+    '--secondary': '#5e503f',
+    '--on-secondary': '#EAE0D5',
+    '--active': '#c6ac8f',
+    '--on-active': '#EAE0D5',
+  }
+};
